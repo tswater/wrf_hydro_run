c2011dae7dbf9737c7ee2b9956abb1658a25bff5
tswater <tyswater@gmail.com>
2020-02-21 13:31:20 -0500

    Fine Tune

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 RUN WRF HYDRO!
 
 REQUIRED: 
-A conda environment with the following packages installed:
+- conda environment with the following packages installed:
 	ncl
 	r-ncdf4
 	netcdf4
@@ -11,23 +11,28 @@ A conda environment with the following packages installed:
 	r-plyr
 	rasterio
 	gdal
-Compiled WRF and WRF hydro
-Compiled WPS
-Compiled TauDEM (and gdal library added to LD_LIBRARY_PATH)
-a DEM
-Forcing data
+- Compiled WRF and WRF hydro
+- Compiled WPS
+- Compiled TauDEM (and gdal library added to LD_LIBRARY_PATH)
+- A DEM
+- Forcing data
+
+
 
 First, fill in the text on the wrf_preprocess.py file in the top section. No
 other changes should be necessary in this file. 
 
 Run wrf_preprocess.py
 
-check the output in the DOMAIN and FORCING folders. In DOMAIN you should have
+Check the output in the DOMAIN and FORCING folders. In DOMAIN you should have
 4 netcdf files, metadata, fulldom_hires, wrfinput and geo_emd01.nc
 
 In the Forcing folder, there should be many files
 
 Add the working folder to the path. (export PATH=$PATH:[run directory])
 
-Run run_wrf.py
+Run run_wrf.py [n]  where n is the number of processes requested
 
+All output should be in the OUT folder created by running run_wrf.py
+
+To clean the output AND preprocessing from the folder, run scripts/clean.py
--- scripts/clean.py
+++ scripts/clean.py
@@ -1,30 +1,16 @@
 # Completely clean out the run folder and return it to an 'empty' state
 import subprocess
 
-run_dir ='../'
-w_dir = '../workspace'
-namelist_dir = '../namelists/'
-log_dir = '../logs'
-dom_dir = '../DOMAIN'
-forc_dir = '../FORCING'
-try :
-	subprocess.run('rm -r '+log_dir,shell=True)
-except:
-	print('No such Files in '+log_dir)
-try:
-	subprocess.run('rm '+namelist_dir+'*',shell=True)
-except:
-	print('No such Files in '+namelist_dir)
-try:
-	subprocess.run('rm -r '+workspace,shell=True)
-except:
-	print('ERROR: cannot remove workspace or does not exist')
-try: 
-	subprocess.run('rm -r -i '+dom_dir,shell=True)
-except: 
-	print('ERROR: cannot remove DOMAIN or does not exist')
-try:
-	subprocess.run('rm -r -i '+forc_dir,shell=True)
-except:
-	print('ERROR: cannot remove FORCING or does not exist')
-
+run_dir ='/home/tsw35/soteria/wrf_hydro/wrf_hydro_run'
+w_dir = run_dir+'/workspace/'
+namelist_dir = run_dir+'/namelists/'
+log_dir = run_dir+'/logs/'
+dom_dir = run_dir+'/DOMAIN/'
+forc_dir = run_dir+'/FORCING/'
+subprocess.run('rm -r '+log_dir,shell=True)
+subprocess.run('rm '+namelist_dir+'*',shell=True)
+subprocess.run('rm -r '+workspace,shell=True)
+subprocess.run('rm -r '+dom_dir,shell=True)
+subprocess.run('rm -r '+forc_dir,shell=True)
+subprocess.run('rm -r '+run_dir+'/OUT/',shell=True)
+subprocess.run('rm '+run_dir+'/*.nc')
